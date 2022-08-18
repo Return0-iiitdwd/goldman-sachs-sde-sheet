dcbb20440a347dbd1030ee0e5a249cb8b87e1192
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2022-08-18 18:58:49 +0530

    day - 6

---
+++ 724_Find_Pivot_Index.cpp
@@ -0,0 +1,24 @@
+//724. Find Pivot Index
+
+
+//time complexity: O(N)
+//space complexity: O(1)
+//logic: use prefix sum method
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        int tempSum = 0;
+        for(auto num: nums)
+        {
+            sum += num;
+        }
+        for(int i = 0;i < nums.size(); i++)
+        {
+            if(tempSum == sum - tempSum - nums[i])
+                return i;
+            tempSum += nums[i];
+        }
+        return -1;
+    }
+};
